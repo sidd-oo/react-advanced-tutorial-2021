826f95257c95513c9390e5a8a5db31a2f8584770
Siddharth Sahoo <sahoosiddharth3@gmail.com>
2021-01-18 21:45:34 +0530

    Complex counter using useState

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -16,6 +16,14 @@ const UseStateCounter = () => {
     setCounter(0);
   }
 
+  const incrementLaterCount = ()=>{
+    setTimeout(()=>{
+      setCounter((counter)=>{
+        return counter + 1;
+      });
+    },2000);
+  }
+
   return <>
       <h2>useState counter example</h2>;
       <section style = {{margin :'4rem 0'}}>
@@ -25,6 +33,12 @@ const UseStateCounter = () => {
           <button className='btn' onClick={resetCount}>Reset</button>
           <button className='btn' onClick={decrementCount}>Decrement</button>
       </section>
+
+    <section style={{ margin: '4rem 0' }}>
+      <h2>Complex Counter</h2>
+      <h1>{counter}</h1>
+      <button className='btn' onClick={incrementLaterCount}>Increment Later</button>
+    </section>
     </>
 };
 
